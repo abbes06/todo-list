2f408da5f7e2016e4865dbe8f3ef09c71981dad3
abbes <benayache.abbes@gmail.com>
2020-12-20 23:23:55 +0100

    affichage des todos

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import "./style.css";
+
+const ul = document.querySelector("ul");
+
+const todos = [
+  {
+    text: "je suis une todo",
+    done: false,
+  },
+  {
+    text: "faire du javascript",
+    done: true,
+  },
+];
+
+const displayTodo = () => {
+  const todoNodes = todos.map((todo, index) => {
+    return createElement(todo);
+  });
+  ul.innerHTML = "";
+  ul.append(...todoNodes);
+};
+const createElement = (todo) => {
+  const li = document.createElement("li");
+  console.log(todo.done);
+  li.innerHTML = `<span class = 'todo ${todo.done ? "done" : ""}'></span> 
+  <p>${todo.text}</p>
+  <button>Editer</button>
+  <button>Supprimer</button>`;
+  console.log(li);
+  return li;
+};
+
+displayTodo();
